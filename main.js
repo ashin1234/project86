5db4c2bb86dc8453c7e470a7caa6099323fed62a
ashin1234 <78942006+ashin1234@users.noreply.github.com>
2021-07-05 19:11:25 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 var canvas = new fabric.Canvas('myCanvas');
  var x= document.getElementById("myAudio");
 
+
 function new_image()
 {  fabric.Image.fromURL("Birthdayimage.jpg", function(img)
 {
     block_image_objective=img;
 
-    block_image_objective.scaleToWidth(700);
-    block_image_objective.scaleToHeight(510);
+    block_image_objective.scaleToWidth(600);
+    block_image_objective.scaleToHeight(410);
     block_image_objective.set({
         top:0,
         left:0
@@ -19,5 +20,6 @@ function new_image()
 
 function playSound(){
     x.play();
+    
 	
 }
